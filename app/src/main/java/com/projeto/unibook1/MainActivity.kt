465dc76d792f8aed12dd7e219cdd398065218df0
Pedro Lucas de Oliveira Duarte <pedrooliveiraa25@gmail.com>
2026-04-07 19:23:30 -0300

    tela-cadastro

--- app/src/main/java/com/projeto/unibook1/MainActivity.kt
+++ app/src/main/java/com/projeto/unibook1/MainActivity.kt
@@ -5,14 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.projeto.unibook1.ui.admin.AdminLoginScreen // Importa a tela que criamos
 import com.projeto.unibook1.ui.theme.Unibook1Theme
+import com.projeto.unibook1.usuario.mapa.CadastroScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Unibook1Theme {
-                // Aqui você chama a função que criou na pasta admin
                 AdminLoginScreen()
+                CadastroScreen(
+                    onNavigateToLogin = {},
+                    onNavigateToSuporte = {}
+                )
             }
         }
     }
