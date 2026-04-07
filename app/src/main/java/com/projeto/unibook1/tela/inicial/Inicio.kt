060e87ed35b7b4a7aef850960d56ee40ca710a27
Caique Mendonça <132861061+Caique-Mendonca@users.noreply.github.com>
2026-04-07 19:22:55 -0300

    Update Inicio.kt

--- app/src/main/java/com/projeto/unibook1/tela/inicial/Inicio.kt
+++ app/src/main/java/com/projeto/unibook1/tela/inicial/Inicio.kt
@@ -1,2 +1,11 @@
 package com.projeto.unibook1.tela.inicial
 
+import android.R
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TelaInicial() {
+    Text(text = "Olá, Narak", style = MaterialTheme.typography.headlineMedium)
+}
