74b8fa973dece2c2aa4a0096a8dc18eb9fa69cf4
Caique Mendonça <132861061+Caique-Mendonca@users.noreply.github.com>
2026-04-08 17:13:37 -0300

    LIXO LIXO LIXO VTMNC

--- app/src/main/java/com/projeto/unibook1/usuario/Inicio/Inicio.kt
+++ app/src/main/java/com/projeto/unibook1/usuario/Inicio/Inicio.kt
@@ -1,10 +1,33 @@
 package com.projeto.unibook1.usuario.Inicio
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
+class MainActivity: ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TelaInicial()
+        }
+    }
+}
 @Composable
 fun TelaInicial() {
-    Text(text = "Olá, Narak", style = MaterialTheme.typography.headlineMedium)
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Olá, Narak", style = MaterialTheme.typography.headlineMedium)
+    }
+
 }
+
