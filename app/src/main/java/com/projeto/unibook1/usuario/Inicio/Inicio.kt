a310cc57444ec0e014f45555494772cfff7ee873
Caique Mendonça <132861061+Caique-Mendonca@users.noreply.github.com>
2026-04-08 18:06:42 -0300

    tela incio

--- app/src/main/java/com/projeto/unibook1/usuario/Inicio/Inicio.kt
+++ app/src/main/java/com/projeto/unibook1/usuario/Inicio/Inicio.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity: ComponentActivity(){
@@ -17,17 +27,43 @@ class MainActivity: ComponentActivity(){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TelaInicial()
+            TelaInicial(onReservaClick = {})
         }
     }
 }
 @Composable
-fun TelaInicial() {
+fun TelaInicial(onReservaClick: () -> Unit) {
+    var nomeAluno by remember { mutableStateOf("Nome do Aluno") }
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "Olá, Narak", style = MaterialTheme.typography.headlineMedium)
+        Row(
+            modifier = Modifier,
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(text = "\uD83D\uDCD6\u200B ")
+            Text(text = "Unifriend",
+                style = MaterialTheme.typography.headlineLarge,
+                color = Color(0xFF1976D2),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+
+            )
+
+        }
+        Text(text = "Olá, $nomeAluno", style = MaterialTheme.typography.headlineMedium)
+        Button(onClick = onReservaClick) {
+            Text(text = "Clique aqui")
+        }
     }
 
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MapScreenPreview() {
+    TelaInicial(onReservaClick = {})
 }
 
