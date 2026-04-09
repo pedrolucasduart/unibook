72ac2402e2a0acb8da512514716efa690e6eff18
Caique Mendonça <132861061+Caique-Mendonca@users.noreply.github.com>
2026-04-09 18:43:11 -0300

    Tela inicial andamento

--- app/src/main/java/com/projeto/unibook1/usuario/Inicio/Inicio.kt
+++ app/src/main/java/com/projeto/unibook1/usuario/Inicio/Inicio.kt
@@ -1,20 +1,30 @@
 package com.projeto.unibook1.usuario.Inicio
 
-import android.R
-import android.graphics.Color.green
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -44,28 +54,30 @@ class MainActivity: ComponentActivity(){
 @Composable
 fun TelaInicial(onReservaClick: () -> Unit) {
     var nomeAluno by remember { mutableStateOf("Nome do Aluno") }
+    var livrosAtivos by remember { mutableStateOf("3") }
+    var totalReservas by remember { mutableStateOf("1") }
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().background(Color(0xFFF6F6F9)).padding(14.dp)
     ) {
+        Spacer(modifier = Modifier.height(24.dp))
         Row(
-            modifier = Modifier,
+            modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(text = "\uD83D\uDCD6\u200B ")
-            Text(text = "Unifriend",
+            Text(text = "\uD83D\uDCD6\u200B Unifriend",
                 style = MaterialTheme.typography.headlineLarge,
                 color = Color(0xFF1976D2),
                 fontWeight = FontWeight.Bold
 
             )
             Button(
-                onClick = { onReservaClick() },
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1976D2)),
-                modifier = Modifier.size(48.dp).background(color = Color(0xFF1976D2))
+                onClick = { /* Ação de abrir notificações*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFF6F6F9)),
+
             ){
                 Text(
-                    text = "Notificações",
+                    text = "\uD83D\uDD14\u200B",
                     style = MaterialTheme.typography.headlineSmall,
                     color = Color.Gray
                 )
@@ -73,8 +85,177 @@ fun TelaInicial(onReservaClick: () -> Unit) {
             }
 
         }
-        Text(text = "Olá, $nomeAluno! 👋", style = MaterialTheme.typography.headlineMedium)
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Olá, $nomeAluno! 👋",
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold
+            )
         Text(text = "Bem-vindo à biblioteca da Unifor!")
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            placeholder = { Text("\uD83D\uDD0D\u200B Livro, autor, assunto ou professor...") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .background(Color.White)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            border = BorderStroke(1.dp, Color(0xFFF0F0F0)),
+            colors = CardDefaults.cardColors(containerColor = Color.White)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = "EMPRÉSTIMOS E RESERVAS",
+                    color = Color(0xFF1976D2),
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.labelLarge,
+                )
+                
+                Spacer(modifier = Modifier.height(16.dp))
+                
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(modifier = Modifier.weight(1f)) {
+                        Text(
+                            text = if (livrosAtivos.length == 1) "0$livrosAtivos" else livrosAtivos,
+                            style = MaterialTheme.typography.headlineMedium,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0xFF1C274C)
+                        )
+                        Text(
+                            text = "Livros\nAtivos",
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color(0xFF5C616F)
+                        )
+                    }
+
+                    // Divisor vertical
+                    Box(
+                        modifier = Modifier
+                            .height(45.dp)
+                            .width(1.dp)
+                            .background(Color(0xFFE0E0E0))
+                    )
+
+                    Column(modifier = Modifier.weight(1f).padding(start = 24.dp)) {
+                        Text(
+                            text = if (totalReservas.length == 1) "0$totalReservas" else totalReservas,
+                            style = MaterialTheme.typography.headlineMedium,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0xFF8D4B1F)
+                        )
+                        Text(
+                            text = "Reservas",
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color(0xFF5C616F)
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "Ver detalhes →",
+                    color = Color(0xFF1976D2),
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.clickable { onReservaClick() }
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFFF9EFEE)),
+            border = BorderStroke(1.dp, Color(0xFFF2DEDE))
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "ALERTAS RECENTES",
+                        color = Color(0xFF8B1A10),
+                        style = MaterialTheme.typography.labelLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(text = "⚠", color = Color(0xFF8B1A10)) 
+                }
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    text = "Prazo de devolução amanhã",
+                    color = Color(0xFF5D120D),
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text(
+                    text = "Livro: Psicologia Experimental",
+                    color = Color(0xFF8B1A10),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = { /* Ação de renovar */ },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF8B1A10)),
+                    contentPadding = PaddingValues(horizontal = 24.dp, vertical = 8.dp),
+                    modifier = Modifier.height(38.dp)
+                ) {
+                    Text("Renovar", color = Color.White, fontWeight = FontWeight.Bold)
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(text = "Acesso Rápido",
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.labelLarge
+            )
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier.background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Button(
+                    onClick = { /* Ação de renovar */ },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE9F1FB)),
+                    modifier = Modifier.background(color = Color(0xFFE9F1FB))
+                ) {
+                    Text(text = "\uD83D\uDCF7\u200B")
+                }
+
+            }
+            Column() { }
+            Column() { }
+        }
     }
 
 
