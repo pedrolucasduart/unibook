d7eebd45fb13fc5b4f3b6bab0b3556de357cf600
Pedro Lucas de Oliveira Duarte <pedrooliveiraa25@gmail.com>
2026-04-15 22:00:50 -0300

    tela de login adicionada e alteracoes no cadastro

--- app/src/main/java/com/projeto/unibook1/MainActivity.kt
+++ app/src/main/java/com/projeto/unibook1/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.projeto.unibook1
+/*package com.projeto.unibook1
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -16,21 +16,16 @@ import com.projeto.unibook1.usuario.cadastro.CadastroScreen
 import com.projeto.unibook1.usuario.mapa.MapScreen
 import com.projeto.unibook1.telasgerais.TelaReservaArmario
 
-// 👇 NOVOS IMPORTS (Verifique se os pacotes batem com as pastas do seu projeto!)
-import com.projeto.unibook1.admin.AdminMainScreen
-import com.projeto.unibook1.admin.ProfileScreen
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Unibook1Theme {
 
+
                 val navController = rememberNavController()
 
-                // 👇 Coloquei "admin_home" aqui para você testar suas telas logo de cara!
-                // Depois você pode voltar para "mapa" ou "login_admin".
-                NavHost(navController = navController, startDestination = "admin_home") {
+                NavHost(navController = navController, startDestination = "cadastro") {
 
                     // Tela de Login Zíltom
                     composable("login_admin") {
@@ -51,39 +46,17 @@ class MainActivity : ComponentActivity() {
                         TelaReservaArmario()
                     }
 
-                    // 👇 --- NOVAS TELAS DO ADMIN AQUI --- 👇
-
-                    // Tela Principal do Admin (Home)
-                    composable("admin_home") {
-                        AdminMainScreen(
-                            onProfileClick = {
-                                // MÁGICA 1: Vai para a tela de perfil ao clicar na bolinha
-                                navController.navigate("admin_profile")
-                            },
-                            onOpenScannerClick = {
-                                // Lógica futura do seu scanner
-                            },
-                            onStudentClick = { nomeEstudante ->
-                                // Lógica futura ao clicar em um aluno na lista
-                            }
+                    // Tela de Cadastro - Pedro
+                    composable(route = "cadastro") {
+                        CadastroScreen(
+                            onNavigateToLogin = { navController.navigate(route = "mapa") },
+                            onNavigateToSuporte = { }
                         )
                     }
-
-                    // Tela de Perfil do Admin
-                    composable("admin_profile") {
-                        ProfileScreen(
-                            onBackClick = {
-                                // MÁGICA 2: Volta para a tela anterior (Home)
-                                navController.popBackStack()
-                            },
-                            onChangeProfilePictureClick = {
-                                // Lógica futura para abrir a galeria e mudar a foto
-                            }
-                        )
-                    }
-
                 }
             }
         }
     }
 }
+
+ */
--- app/src/main/java/com/projeto/unibook1/usuario/cadastro/Main.kt
+++ app/src/main/java/com/projeto/unibook1/usuario/cadastro/Main.kt
@@ -1,22 +1,45 @@
-/*package com.projeto.unibook1
+package com.projeto.unibook1
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.projeto.unibook1.ui.theme.Unibook1Theme
 import com.projeto.unibook1.usuario.cadastro.CadastroScreen
+import com.projeto.unibook1.usuario.cadastro.LoginAlunoScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                CadastroScreen(
-                    onNavigateToLogin = { },
-                    onNavigateToSuporte = { }
-                )
+            Unibook1Theme {
+
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "login_aluno") {
+
+                    // Tela de Login do Aluno - Pedro
+                    composable(route = "login_aluno") {
+                        LoginAlunoScreen(
+                            onNavigateToCadastro = { navController.navigate(route = "cadastro") },
+                            onNavigateToSuporte = { },
+                            onEsqueceuSenha = { },
+                            onLoginSucesso = { navController.navigate(route = "login_aluno") }
+                        )
+                    }
+
+                    // Tela de Cadastro - Pedro
+                    composable(route = "cadastro") {
+                        CadastroScreen(
+                            onNavigateToLogin = { navController.navigate(route = "login_aluno") },
+                            onNavigateToSuporte = { }
+                        )
+                    }
+
+                }
             }
         }
     }
 }
-*/
